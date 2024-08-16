6bd862fbc2c14f0459df0c8363ccb9af4eba66fb
dilipkumar <dilipofficial11@gmail.com>
2024-08-16 18:56:25 +0530

    Initial Commit

---
+++ Apple-FrameWork/Apple_FrameWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameWorkApp.swift
+//  Apple-FrameWork
+//
+//  Created by dilipkumar on 16/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
